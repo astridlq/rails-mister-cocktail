78fa6c06c25faca67705ae70853f8fb8635ab264
Astrid Le Quere <astrid.lequere@gmail.com>
2019-05-17 12:26:50 +0200

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "/", to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
